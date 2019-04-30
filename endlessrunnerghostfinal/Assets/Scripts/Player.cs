2cc56cd8e57982862703be9affd1e87148e7bd88
Sopita Daniels <43454887+melancholinist@users.noreply.github.com>
2019-04-30 16:38:59 +0700

    game commit

---
+++ endlessrunnerghostfinal/Assets/Scripts/Player.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    private Vector2 targetPos;
+    public float Yincrement;
+    public float speed;
+    public float maxHeight;
+    public float minHeight;
+    public int health = 3;
+
+
+    public Text healthDisplay;
+    public GameObject gameOver;
+   
+
+    private void Update()
+    {
+        healthDisplay.text = health.ToString();
+
+
+        if (health <= 0)
+        {
+            gameOver.SetActive(true);
+            Destroy(gameObject);
+        }
+        transform.position = Vector2.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
+        if (Input.GetKeyDown(KeyCode.UpArrow) && transform.position.y < maxHeight)
+        {
+            targetPos = new Vector2(transform.position.x, transform.position.y + Yincrement);
+            transform.position = targetPos;        
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow) && transform.position.y > minHeight)
+        {
+            targetPos = new Vector2(transform.position.x, transform.position.y - Yincrement);
+            transform.position = targetPos;
+        }
+    }
+}
